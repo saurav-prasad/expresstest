d46b4f3c09a1011d85a245495ef08d66cbb1a216
Saurav <sauravprasad2050@gmail.com>
2023-08-16 10:37:30 +0530

    expt

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import express from 'express'
+import { config } from 'dotenv'
+import cors from 'cors'
+
+config({ path: "./config/config.env" })
 const app = express()
 const port = 3000
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/api/getkey", (req, res) => res.status(200).json({ key: process.env.RAZORPAY_KEY_ID }))
+app.post("/api/postkey", (req, res) => {
+    console.log(req.body);
+    res.status(200).json(req.body)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 app.use('/', (req, res) => {
-    res.json({ status: true })
+    res.json({ success: true })
 })
 app.listen(port, () => {
     console.log("App running at port", port);
