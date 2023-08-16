e274cecde53b5396b069efb658e24dadf02ab85c
Saurav <sauravprasad2050@gmail.com>
2023-08-16 11:07:36 +0530

    razorpay apis built

---
+++ controllers/paymentController.js
@@ -0,0 +1,35 @@
+import { instance } from "../index.js";
+import crypto from 'crypto'
+
+export const checkout = async (req, res) => {
+    console.log("create orderId request", req.body);
+    var options = {
+        amount: Number(req.body.amount * 100),  // amount in the smallest currency unit
+        currency: "INR",
+    };
+    const order = await instance.orders.create(options);
+    console.log("checkout", order);
+    res.status(200).json(order)
+}
+
+export const paymentVerification = (req, res) => {
+    const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body
+
+    let body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+        .update(body.toString())
+        .digest('hex');
+    console.log("sig received", razorpay_signature);
+    console.log("sig generated", expectedSignature);
+    let response = { "signatureIsValid": "false" }
+
+    if (expectedSignature === razorpay_signature) {
+        response = { "signatureIsValid": "true" }
+        console.log("paymentVerification", req.body);
+        res.status(200).json(response)
+    } else {
+        console.log("paymentVerification", req.body);
+        res.status(200).json(response)
+    }
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { config } from 'dotenv'
 import cors from 'cors'
+import router from './routes/paymentRoutes.js'
+import Razorpay from 'razorpay'
+
 
 config({ path: "./config/config.env" })
 const app = express()
@@ -11,16 +14,21 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/api/getkey", (req, res) => res.status(200).json({ key: process.env.RAZORPAY_KEY_ID }))
+
 app.post("/api/postkey", (req, res) => {
     console.log(req.body);
     res.status(200).json(req.body)
 })
 
+app.use("/api", router)
 
-app.use("/", (req, res) => {
-    res.sendFile('index.html' , { root : './'});
+app.use('/', (req, res) => {
+    res.json({ success: true })
 })
 app.listen(port, () => {
     console.log("App running at port", port);
 })
+export const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
