4a88026e81bf6f33811374323e03866c34fc9103
Saurav <sauravprasad2050@gmail.com>
2023-08-18 11:47:54 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,5 +5,5 @@ import { checkout, paymentVerification } from '../controllers/paymentController.
 const router = express()
 router.route('/checkout').post(checkout)
 router.route('/verifypayment').post(paymentVerification)
-
+router.route('/getkey').get((req, res) => res.json({ key: "rzp_test_cHPGqZWYooG6s9" }))
 export default router
